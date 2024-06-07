421cbb7debb6414f34092180d9225fc1ee61cbea
DiusNeus <triponovidiu2002@gmail.com>
2024-06-07 13:59:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Nickname Generator.")
+
+ThreeLetter_name = input("What are the first 3 letters of your name?\n")
+
+Favourite_snack = input("What's the name of your favourite snack?\n")
+
+print("Your nickname could be\n" + ThreeLetter_name + " " + Favourite_snack)
